c0005126b50968c1d3ce4daedaa1197dcd6292ce
SpaceComet <marquessricardo@gmail.com>
2021-09-14 19:58:28 -0400

    Purgin tailwindcss for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-        colors: {
-            gray: colors.trueGray,
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                gray: colors.trueGray,
+            },
+            fontFamily: {
+                'simplifica': ['Simplifica']
+            },
+            height: theme => ({
+                "screen/2": "50vh",
+                "screen/3": "calc(100vh / 3)",
+                "screen/4": "calc(100vh / 4)",
+                "screen/5": "calc(100vh / 5)",
+            }),
         },
-        fontFamily: {
-            'simplifica': ['Simplifica']
-        },
-        height: theme => ({
-            "screen/2": "50vh",
-            "screen/3": "calc(100vh / 3)",
-            "screen/4": "calc(100vh / 4)",
-            "screen/5": "calc(100vh / 5)",
-        }),
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
