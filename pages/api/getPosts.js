40812c7856fcfbd41dd5a26154a849504b6ac5a6
SpaceComet <marquessricardo@gmail.com>
2021-09-15 21:15:42 -0400

    API to get posts

---
+++ pages/api/getPosts.js
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import fsp from 'fs/promises';
+import path from 'path'
+import matter from 'gray-matter'
+///api/getPosts?postName=LeagueofGods.md
+export default async function handler(req, res) {
+    if (req.method === 'GET' && req.query.postName) {
+        const postName = req.query.postName;
+        const postsDirectory = path.join(process.cwd(), 'posts');
+
+        // Read markdown file as string
+        const fullPath = path.join(postsDirectory, postName)
+        console.log(fullPath);
+
+        try {
+            await fsp.access(fullPath, fs.constants.F_OK);
+            const fileContents = await fsp.readFile(fullPath, 'utf8');
+    
+            // Use gray-matter to parse the post metadata section
+            const matterResult = matter(fileContents)
+            return( res.status(200).json({ data: matterResult.data }) );
+        } catch (error) {
+            console.log(error);
+            return( res.status(404).json({ error: "Error: File does not exist."}) );
+        }
+
+    }
+    return( res.status(400).json({ error: "Error: Wrong request."}) );
+}
---
+++ posts/LeagueofGods.md
@@ -0,0 +1,8 @@
+---
+Year: 2016
+Role: Compositor/Dynamics
+Software: After Effects, Maya, Mocha Pro
+Studio: Offworld VFX, DreamMax
+---
+# LOG
+> Building...
